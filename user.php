6a1de58cc2601aa0f6ccaeeb762d44f60f90c438
Devops Developer <100074939+devopsdeveloper1107@users.noreply.github.com>
2023-03-14 11:43:08 +0530

    Update user.php

--- user.php
+++ user.php
@@ -15,8 +15,25 @@ $method = $_SERVER['REQUEST_METHOD'];
 //echo "test----".$method; die;
 switch($method)
 {
-   case "GET":
-    $alluser= mysqli_query($db_conn, "SELECT * FROM tbl_user");
+   case "GET": 
+    $path= explode('/', $_SERVER['REQUEST_URI']);
+
+    if(isset($path[4]) && is_numeric($path[4]))
+    {
+      $json_array= array();
+      $userid= $path[4];
+      
+      $getuserrow= mysqli_query($db_conn, "SELECT * FROM tbl_user WHERE userid='$userid' ");
+      while($userrow= mysqli_fetch_array($getuserrow))
+      {
+       $json_array['rowUserdata']= array('id'=>$userrow['userid'],'username'=>$userrow['username'], 'email'=>$userrow['useremail'], 'status'=>$userrow['status'],);
+      }
+      echo json_encode($json_array['rowUserdata']);
+      return;
+
+    } else { 
+
+    $alluser= mysqli_query($db_conn, "SELECT * FROM tbl_user"); 
     if(mysqli_num_rows($alluser) > 0)
     {
       while($row= mysqli_fetch_array($alluser))
@@ -29,6 +46,8 @@ switch($method)
         echo json_encode(["result"=>"Please check the Data"]); 
         return;
     }
+  }
+   
     break;
 
     case "POST":
@@ -51,6 +70,28 @@ switch($method)
         }
 
         break;
+    
+         case "PUT":
+          $userUpdate= json_decode(file_get_contents("php://input"));
+
+           $userid= $userUpdate->id;
+           $username= $userUpdate->username;
+           $useremail= $userUpdate->email;
+           $status= $userUpdate->status;
+
+           $updateData= mysqli_query($db_conn, "UPDATE tbl_user SET username='$username', useremail='$useremail', status='$status' WHERE userid='$userid'  ");
+           if($updateData)
+           {
+             echo json_encode(["success"=>"User Record Update Successfully"]);
+             return;
+           } else {
+               echo json_encode(["success"=>"Please Check the User Data!"]);
+               return; 
+           }         
+
+          break;
+
+         
 
 }
 
