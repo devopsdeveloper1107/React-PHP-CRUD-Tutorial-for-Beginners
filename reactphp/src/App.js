cce1d4dec838cdda5cd0ebc148dcfd96da48a474
Devops Developer <100074939+devopsdeveloper1107@users.noreply.github.com>
2023-03-17 10:03:19 +0530

    Update App.js

--- reactphp/src/App.js
+++ reactphp/src/App.js
@@ -6,6 +6,8 @@ import Home from './Component/Home';
 import Userlist from './Component/Userlist';
 import Adduser from './Component/Adduser';
 import Edituser from './Component/Edituser';
+import Addproduct from './Component/Addproduct';
+import Productlist from './Component/Productlist';
 import Footer from './Component/Footer';
 
 function App() {
@@ -17,6 +19,8 @@ function App() {
       <Route path="/userlist" element= { <Userlist/> } />
       <Route path="/adduser" element= { <Adduser/> } />
       <Route path="/edituser/:id" element= { <Edituser/> } />
+      <Route path="/addproduct/" element= { <Addproduct/> } />
+      <Route path="/productlist/" element= { <Productlist/> } />
 
       </Routes>
       <Footer/>    
@@ -25,3 +29,4 @@ function App() {
 }
 
 export default App;
+
